02787238587f7a4bdff25390f5c455c480c6b17f
George Munday <geogeM1@icloud.com>
2026-09-16 23:54:16 +0100

    fix: ensure input value is correctly set to an empty string when not provided

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -20,7 +20,7 @@ const Input = ({ name, type, placeholder, required, search, value, onChange, rea
       type={type}
       placeholder={placeholder}
       required={required}
-      value={hasValue ? value : ''}
+      {...(hasValue ? { value } : {})}
       onChange={onChange}
       readOnly={readOnly ?? (!onChange && hasValue)}
       className={`${search ? 'px-10' : 'px-4'} w-full max-w-xl rounded-md border border-gray-300 py-2 focus:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 text-black`}
